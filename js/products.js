4a0f8e731b81e503574d0a94b01f06086f1eea5b
katie <katiequin335@gmail.com>
2015-06-22 11:30:24 -0400

    upload

---
+++ js/products.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+    $("#DesignEssentials").click(function(){
+        $(".KeraCare, .Mizani, .StraightRequest, .NuExpressions, .Dudleys, .Kemet, .Syntonics, .IsabellasOwn").hide();
+        $(".DesignEssentials").show();
+    });
+});
+
+ $(document).ready(function(){
+   
+    $("#Dudleys").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .StraightRequest, .NuExpressions, .Kemet, .Syntonics, .IsabellasOwn").hide();
+        $(".Dudleys").show();
+    });
+});
+
+ $(document).ready(function(){
+   
+    $("#IsabellasOwn").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .StraightRequest, .NuExpressions, .Kemet, .Syntonics, .Dudleys").hide();
+        $(".IsabellasOwn").show();
+    });
+});
+ 
+$(document).ready(function(){
+   
+    $("#Kemet").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .StraightRequest, .NuExpressions, .Dudleys, .Syntonics, .IsabellasOwn").hide();
+        $(".Kemet").show();
+    });
+});   
+
+$(document).ready(function(){
+   
+    $("#KeraCare").click(function(){
+        $(".DesignEssentials, .Mizani, .StraightRequest, .NuExpressions, .Dudleys, .Kemet, .Syntonics, .IsabellasOwn").hide();
+        $(".KeraCare").show();
+    });
+}); 
+
+$(document).ready(function(){
+   
+    $("#Mizani").click(function(){
+        $(".DesignEssentials, .KeraCare, .StraightRequest, .NuExpressions, .Dudleys, .Kemet, .Syntonics, .IsabellasOwn").hide();
+        $(".Mizani").show();
+    });
+}); 
+
+$(document).ready(function(){
+   
+    $("#NuExpressions").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .StraightRequest, .Dudleys, .Kemet, .Syntonics, .IsabellasOwn").hide();
+        $(".NuExpressions").show();
+    });
+}); 
+
+$(document).ready(function(){
+   
+    $("#StraightRequest").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .NuExpressions, .Dudleys, .Kemet, .Syntonics, .IsabellasOwn").hide();
+        $(".StraightRequest").show();
+    });
+}); 
+
+
+$(document).ready(function(){
+   
+    $("#Syntonics").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .StraightRequest, .NuExpressions, .Dudleys, .Kemet, .IsabellasOwn").hide();
+        $(".Syntonics").show();
+    });
+}); 
+    
+$(document).ready(function(){
+    $("#sales").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .StraightRequest, .NuExpressions, .Dudleys, .Kemet, .Syntonics, .IsabellasOwn").hide();
+        $(".Sales").show();
+    });
+});
+  
+$(document).ready(function(){
+   $("#show").click(function(){
+        $(".DesignEssentials, .KeraCare, .Mizani, .StraightRequest, .NuExpressions, .Dudleys, .Kemet, .Syntonics, .IsabellasOwn").show();
+    });
+});
+
+    
